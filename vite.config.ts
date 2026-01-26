1b3a28ea8b0123376bec455ba8822e12d4128465
Xiang-yu-Wang <sean8411@gmail.com>
2026-01-26 13:35:56 +0800

    fix: 修正 Vite 基本路徑設定格式

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: ".",
+  base: "./",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
