36c270fe4d729c6496caf077f8115dbbdf413e11
Xiang-yu-Wang <124881761+Xiang-yu-Wang@users.noreply.github.com>
2026-01-27 04:45:33 +0000

    Deploying to gh-pages from @ Xiang-yu-Wang/-------_202601221138_XY_Propose_Minutes@ae083de37bf4c17079be741cb811a0b043689b3a 🚀

---
+++ assets/ServicesView-smtAIgPJ.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./ServicesSection.vue_vue_type_script_setup_true_lang-DdQs73CG.js";import{d as o,a as s,e as t,o as a}from"./index-BGj5VbzK.js";const c={class:"pt-16"},p=o({__name:"ServicesView",setup(r){return(_,n)=>(a(),s("div",c,[t(e)]))}});export{p as default};
